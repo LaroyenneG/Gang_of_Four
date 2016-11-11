75d6a6431f67ed5e3ab6ab11a1d7d4b2170d753d
LaroyenneG <guillaume.laroyenne@outlook.com>
2016-11-11 23:30:00 +0100

    + le test ;-)

---
+++ test/Model/TestCarte.java
@@ -0,0 +1,14 @@
+package Model;
+
+import org.junit.Test;
+
+/**
+ * Created by guillaume on 11/11/16.
+ */
+public class TestCarte {
+    @Test
+    public static void testConstructor(){
+
+    }
+
+}
