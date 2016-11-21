540560210383a3dc3d2c53dbfeecf897373569c1
FlorianVaissiere <florian.vaissiere@orange.fr>
2016-11-21 10:05:54 +0100

    Vue, Fond, Barre Menu Fontionne

---
+++ src/Appli.java
@@ -0,0 +1,13 @@
+import Controleur.ControlGroup;
+
+import java.io.IOException;
+
+/**
+ * Created by Florian Vaissiere on 21/11/2016.
+ */
+public class Appli {
+    public static void main(String[] args) throws IOException {
+
+        ControlGroup controlGroup = new ControlGroup();
+    }
+}
---
+++ src/Controleur/Control.java
@@ -0,0 +1,16 @@
+package Controleur;
+
+import Vue.Fenetre;
+
+/**
+ * Created by Florian Vaissiere on 21/11/2016.
+ */
+public class Control {
+
+    Fenetre fenetre;
+
+    protected Control(Fenetre fenetre) {
+
+        this.fenetre = fenetre;
+    }
+}
