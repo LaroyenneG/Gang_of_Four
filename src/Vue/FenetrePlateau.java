8a548368417e2e61c23763e80e0fa479588453c8
Jeremy Renaud <jeremy.renaud@etu.univ-tours.fr>
2016-12-01 16:30:42 +0100

    Ajout des cartes adverses
    Ajouts du visuel des cartes adverses,
    Ajouts des compteurs de cartes adverses,
    Correctif de l'affichage : passage en fonction de la taille de l'écran

--- src/Vue/FenetrePlateau.java
+++ src/Vue/FenetrePlateau.java
@@ -41,6 +41,7 @@ public class FenetrePlateau extends JPanel{
 
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        int posX = ((((X-200))-5*game.getTabJoueurIndex(0).getMain().size())/game.getTabJoueurIndex(0).getMain().size());
 
         passertour.setBounds((int) (11 / 30.0 * X), (int) (13.5 / 15.0 * Y), (int) (1 / 4.0 * X), (int) (1 / 12.0 * Y));
         passertour.setBackground(new Color(0, 0, 0, 0));
@@ -52,11 +53,24 @@ public class FenetrePlateau extends JPanel{
         Image img = getToolkit().getImage("image/fondplateau.jpg");
         g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
 
+
+        Image imgJ2 = getToolkit().getImage("cartes/carteDos.jpg");
+        g.drawImage(imgJ2,  (int)(X*0.1), 400, posX, (int) (posX*1.5), this);
+        g.drawImage(imgJ2, (int)(X*0.85),400, posX, (int) (posX*1.5), this);
+        g.drawImage(imgJ2, (X/2-(posX/2)), 100, posX, (int) (posX*1.5), this);
+
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 20));;
+        g.drawString((Integer.toString(game.getTabJoueurIndex(1).getMain().size())),(int)(X*0.1)+posX,400);
+        g.drawString((Integer.toString(game.getTabJoueurIndex(2).getMain().size())),(int)(X*0.85)+posX,400);
+        g.drawString((Integer.toString(game.getTabJoueurIndex(3).getMain().size())),(X/2-(posX/2))+posX, 100);
+
+
         for (int i=0; i<game.getTabJoueurIndex(0).getMain().size();i++) // on récupére la taille de la main du j1 de game
         {
             // pour chaque cartes en main
-            Image imgi = getToolkit().getImage("cartes/"+game.getTabJoueurIndex(0).getMain().get(i).getFileName()); // on récup le nom de la carte
-            g.drawImage(imgi, (100+(105*i)), 800, 100, 150, this); // on dessine
+            Image imgi = getToolkit().getImage("cartes/"+game.getTabJoueurIndex(0).getMain().get(i).getFileName());// on récup le nom de la carte
+            g.drawImage(imgi, (posX+((posX+5)*i)), (int)(Y*0.77), posX, (int) (posX*1.5), this); // on dessine
         }
 
     }
