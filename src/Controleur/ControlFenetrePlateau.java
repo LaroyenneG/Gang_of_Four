8acc33f6efbc735257ea05766ad617b76bf2367f
FlorianVaissiere <florian.vaissiere@orange.fr>
2017-01-04 12:26:46 +0100

    Meilleur fonctionnement combinaison avec 1Multi

--- src/Controleur/ControlFenetrePlateau.java
+++ src/Controleur/ControlFenetrePlateau.java
@@ -44,7 +44,7 @@ public class ControlFenetrePlateau extends Control implements ActionListener {
                         fenetre.panelFenetrePlateau.cartesChoixMulti[j].setVisible(true);
                         fenetre.autorisationDessiner = true;
                         select1Multi = true;
-                        game.getTabJoueur()[0].getCombinaisonEnCours().remove(unMulti);
+                        game.getTabJoueur()[0].getCombinaisonEnCours().remove(0);
                     }
                     changerVue();
                 }
