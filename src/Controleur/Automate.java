50a8f1e38bdc034f539b044280cd825fa3c16d66
LaroyenneG <laroyenneg@gmail.com>
2017-01-05 21:53:17 +0100

    modif pour compilation sous openjdk7

--- src/Controleur/Automate.java
+++ src/Controleur/Automate.java
@@ -15,12 +15,14 @@ public class Automate {
 
     private Thread automate;
 
-    private boolean wait;
+    private final boolean[] wait;
 
     public Automate(Control control){
         this.control=control;
         game=control.game;
-        wait=true;
+
+        wait= new boolean[1];
+        wait[0]=false;
     }
 
     public void auto(){
@@ -52,7 +54,7 @@ public class Automate {
     }
 
     private void actionIA(){
-        wait=true;
+        wait[0]=true;
 
         while (game.getJoueurPlay()!=0){
             try {
@@ -64,7 +66,7 @@ public class Automate {
             game.nextJoueur();
             control.changerVue();
 
-            for (int t=0; t<60&&wait; t++){
+            for (int t=0; t<60 && wait[0]; t++){
                 try {
                     sleep(100);
                 } catch (InterruptedException e) {
@@ -78,8 +80,7 @@ public class Automate {
     annule le timer
      */
     public void stopWaitIA(){
-        wait=false;
-
+        wait[0]=false;
     }
 
 }
