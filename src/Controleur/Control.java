a0c73eed60f59d39f754beaf788e28ab4869a2aa
Jeremy Renaud <jeremy.renaud@etu.univ-tours.fr>
2017-01-10 20:19:36 +0100

    modif du score (  affichage )

--- src/Controleur/Control.java
+++ src/Controleur/Control.java
@@ -1,9 +1,8 @@
 package Controleur;
 
 import Model.Game;
+import Model.IA;
 import Vue.Fenetre;
-
-import static java.lang.Thread.sleep;
 
 /**
  * Created by Florian Vaissiere on 21/11/2016.
@@ -27,6 +26,13 @@ public abstract class Control {
         fenetre.pack();
         fenetre.setLocationRelativeTo(null);
         fenetre.requestFocus();
+        IA ia1 = (IA) game.getTabJoueur()[1];
+        IA ia2 = (IA) game.getTabJoueur()[2];
+        IA ia3 = (IA) game.getTabJoueur()[3];
+        fenetre.barreMenu.scorej1.setText("Vous : "  + game.getTabJoueur()[0].getScore());
+        fenetre.barreMenu.scorej2.setText(ia1.getName()+": " + game.getTabJoueur()[1].getScore());
+        fenetre.barreMenu.scorej3.setText(ia2.getName()+": " + game.getTabJoueur()[2].getScore());
+        fenetre.barreMenu.scorej4.setText(ia3.getName()+": " + game.getTabJoueur()[3].getScore());
         if(game.getTabJoueurIndex(0).peutJouer){
             fenetre.panelFenetrePlateau.passertour.setText("Passer le tour");
         }else {
