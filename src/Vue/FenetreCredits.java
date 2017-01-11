247f0295780c014bd4875130da8dfcea96d6915f
FlorianVaissiere <florian.vaissiere@orange.fr>
2017-01-11 12:39:52 +0100

    Retour visible dans fenetre crédit

--- src/Vue/FenetreCredits.java
+++ src/Vue/FenetreCredits.java
@@ -42,9 +42,13 @@ public class FenetreCredits extends JPanel{
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        Font f=new Font("Arial", Font.BOLD, 18);
+
         retour.setBounds((int) (11 / 30.0 * X), (int) (12.5 / 15.0 * Y), (int) (1 / 4.0 * X), (int) (1 / 12.0 * Y));
         retour.setBackground(new Color(0, 0, 0, 0));
         retour.setFocusable(false);
+        retour.setFont(f);
+        retour.setForeground(Color.WHITE);
         retour.setCursor(new Cursor(Cursor.HAND_CURSOR));
         retour.setBorder(null);
 
